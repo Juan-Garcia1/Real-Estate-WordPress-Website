7467b6f2037b34a6ea9d479c731239044953f659
Juan Garcia <juan_garcia_1995@hotmail.com>
2019-01-12 18:02:03 -0600

    Update header.php

--- header.php
+++ header.php
@@ -4,7 +4,15 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
+  <title><?php wp_title(""); ?></title>
   <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
+
+<?php if ( is_front_page() || is_search() ) : ?>
+  <header class="jumbotron jumbotron-fluid" style="background: linear-gradient(0, rgba(0,0,0,.7), rgba(0,0,0,.6)), url('<?php echo content_url() . '/uploads/2019/01/main-header-img.jpeg' ?>') no-repeat center center / cover; padding: 15% 0;">
+    <div class="container">
+      <?php get_search_form(); ?>
+    </div>
+  </header>
+<?php endif; ?>
