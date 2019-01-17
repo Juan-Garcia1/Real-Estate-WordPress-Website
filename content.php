b255b3e3bc7da8065427069d607f102df721ce13
Juan Garcia <juan_garcia_1995@hotmail.com>
2019-01-17 13:47:19 -0600

    Made code less dry. Added type of furnishing for each listing.

--- content.php
+++ content.php
@@ -1,35 +1,21 @@
-<?php if ( is_search() ) : ?>
-  <div class="col-md-4 p-3">
+<?php 
+  $terms = get_the_terms( $post->ID , 'furnishing' );
+?>
+
+
+  <div class="<?php if(is_search() || is_archive()) { echo "col-md-4 "; } else { echo "col-sm-6 "; } ?> p-3">
     <article>
+      <div>
         <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail("large"); ?></a>
-        <h5><?php the_title(); ?></h5>
-        <span class="small">
-          <?php
-            $terms = get_the_terms( $post->ID , 'furnishing' );
-            foreach ( $terms as $term ) {
-              echo '<a href="' . get_term_link($term->slug, 'furnishing') . '">' . $term->name . '</a>';
-            }
-          ?>        
-        </span>
-      </article>
-  </div>
-  <!-- col-md-4 -->
-<?php else : ?>
-  <div class="col-sm-6 p-3">
-    <article>
-        <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail("large"); ?></a>
-        <h5><?php the_title(); ?></h5>
-        <?php if (!is_archive()) : ?>
+        <?php if ( !is_archive() ) : ?>
           <span class="small">
-          <?php
-            $terms = get_the_terms( $post->ID , 'furnishing' );
-            foreach ( $terms as $term ) {
-              echo '<a href="' . get_term_link($term->slug, 'furnishing') . '">' . $term->name . '</a>';
-            }
-            ?>        
+            <?php foreach ( $terms as $term ) : ?>
+              <?php echo $term->name; ?>
+            <?php endforeach; ?>        
           </span>
         <?php endif; ?>
-      </article>
+      </div>
+      <h5><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
+    </article>
   </div>
-  <!-- col-sm-6 -->
-<?php endif; ?>
+  <!-- col -->
